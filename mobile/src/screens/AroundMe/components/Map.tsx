1bcc1223dbc1d1be26d6167eb55e3e6ffd14aae3
duranhumes <duranhumes@gmail.com>
2019-01-27 14:09:41 -0500

    Added custom maps server tiles

--- mobile/src/screens/AroundMe/components/Map.tsx
+++ mobile/src/screens/AroundMe/components/Map.tsx
@@ -6,15 +6,20 @@ import {
     Animated,
     TouchableHighlight,
 } from 'react-native';
-import MapView, { Marker, Callout, Region } from 'react-native-maps';
+import MapView, { Marker, Callout, Region, UrlTile } from 'react-native-maps';
+
+import { IMarker } from '../interfaces';
 
 interface IMap {
     _ref: any;
     region: Region;
-    markers: object[];
+    markers: IMarker[];
     updateUserLocation: (...args: any) => void;
     interpolations: object;
 }
+
+const mapUrl =
+    'https://maps.bahamaslocal.com/styles/klokantech-basic/{z}/{x}/{y}.png';
 
 export function Map({
     _ref,
@@ -24,67 +29,82 @@ export function Map({
     interpolations,
 }: IMap) {
     return (
-        <MapView
-            ref={_ref}
-            region={region}
-            showsUserLocation={true}
-            onRegionChange={updateUserLocation}
-            style={styles.container}>
-            {markers.map((marker: any, index) => {
-                const scaleStyle = {
-                    transform: [
-                        {
-                            scale: interpolations[index].scale,
-                        },
-                    ],
-                };
-                const opacityStyle = {
-                    opacity: interpolations[index].opacity,
-                };
-                const { id, latitude, longitude } = marker;
+        <View style={styles.container}>
+            <MapView
+                ref={_ref}
+                region={region}
+                showsUserLocation={true}
+                onRegionChange={updateUserLocation}
+                mapType="none"
+                provider={null}
+                style={styles.map}>
+                <UrlTile urlTemplate={mapUrl} maximumZ={17} />
+                {markers.map((marker: { [key: string]: any }, index) => {
+                    const scaleStyle = {
+                        transform: [
+                            {
+                                scale: interpolations[index].scale,
+                            },
+                        ],
+                    };
+                    const opacityStyle = {
+                        opacity: interpolations[index].opacity,
+                    };
+                    const { id, latitude, longitude } = marker;
 
-                return (
-                    <Marker
-                        key={id}
-                        coordinate={{
-                            latitude,
-                            longitude,
-                        }}>
-                        <Animated.View
-                            style={[styles.markerWrap, opacityStyle]}>
-                            <Animated.View style={[styles.ring, scaleStyle]} />
-                            <View style={styles.markerIcon} />
-                        </Animated.View>
-                        <Callout>
-                            <TouchableHighlight
-                                onPress={console.log}
-                                underlayColor="#dddddd">
-                                <View style={styles.calloutText}>
-                                    <Text numberOfLines={1}>
-                                        {marker.title}
-                                    </Text>
-                                    <Text numberOfLines={1}>
-                                        {marker.address}
-                                    </Text>
-                                    <Text numberOfLines={1}>
-                                        {marker.phone}
-                                    </Text>
-                                    <Text numberOfLines={1}>
-                                        {marker.distance.toFixed(2)} miles away
-                                    </Text>
-                                </View>
-                            </TouchableHighlight>
-                        </Callout>
-                    </Marker>
-                );
-            })}
-        </MapView>
+                    return (
+                        <Marker
+                            key={id}
+                            coordinate={{
+                                latitude,
+                                longitude,
+                            }}>
+                            <Animated.View
+                                style={[styles.markerWrap, opacityStyle]}>
+                                <Animated.View
+                                    style={[styles.ring, scaleStyle]}
+                                />
+                                <View style={styles.markerIcon} />
+                            </Animated.View>
+                            <Callout>
+                                <TouchableHighlight
+                                    onPress={console.log}
+                                    underlayColor="#dddddd">
+                                    <View style={styles.calloutText}>
+                                        <Text numberOfLines={1}>
+                                            {marker.title}
+                                        </Text>
+                                        <Text numberOfLines={1}>
+                                            {marker.address}
+                                        </Text>
+                                        <Text numberOfLines={1}>
+                                            {marker.phone}
+                                        </Text>
+                                        <Text numberOfLines={1}>
+                                            {marker.distance.toFixed(2)} miles
+                                            away
+                                        </Text>
+                                    </View>
+                                </TouchableHighlight>
+                            </Callout>
+                        </Marker>
+                    );
+                })}
+            </MapView>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    map: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
     },
     markerWrap: {
         alignItems: 'center',
