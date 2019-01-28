3d5f9f59c16ee8994950f9c64622387295d8a692
duranhumes <duranhumes@gmail.com>
2019-01-28 13:08:41 -0500

    Reset card slider index on updates
    Using cDU to update the slider index on changes like a category or distance change, as before it would stay at the previous index and have cards to the left although its a new list of items.

--- mobile/src/screens/AroundMe/components/CardList.tsx
+++ mobile/src/screens/AroundMe/components/CardList.tsx
@@ -6,47 +6,57 @@ import { Card } from './Card';
 import { CARD_WIDTH, VIEWPORT_WIDTH } from '../constants';
 import { IMarker } from '../interfaces';
 
-interface ICardList {
+interface ICardListProps {
     markers: IMarker[];
     animation: Animated.Value;
     handleNavigation: (...args: any) => void;
     handleCall: (...args: any) => void;
 }
 
-export function CardList({
-    markers,
-    animation,
-    handleNavigation,
-    handleCall,
-}: ICardList) {
-    return (
-        <Carousel
-            data={markers}
-            renderItem={({ item }: any) => (
-                <Card
-                    item={item}
-                    handleNavigation={handleNavigation}
-                    handleCall={handleCall}
-                />
-            )}
-            onScroll={Animated.event(
-                [
-                    {
-                        nativeEvent: {
-                            contentOffset: {
-                                x: animation,
+export class CardList extends React.Component<ICardListProps> {
+    _carousel: any;
+
+    componentDidUpdate() {
+        // Reset slider current index to 0
+        this._carousel.snapToItem(0, false);
+    }
+
+    render() {
+        const { markers, animation, handleNavigation, handleCall } = this.props;
+
+        return (
+            <Carousel
+                ref={(ref: any) => (this._carousel = ref)}
+                data={markers}
+                renderItem={({ item }: any) => (
+                    <Card
+                        item={item}
+                        handleNavigation={handleNavigation}
+                        handleCall={handleCall}
+                    />
+                )}
+                onScroll={Animated.event(
+                    [
+                        {
+                            nativeEvent: {
+                                contentOffset: {
+                                    x: animation,
+                                },
                             },
                         },
-                    },
-                ],
-                { useNativeDriver: true }
-            )}
-            sliderWidth={VIEWPORT_WIDTH}
-            itemWidth={CARD_WIDTH}
-            containerCustomStyle={styles.scrollView}
-            activeAnimationType={'spring'}
-        />
-    );
+                    ],
+                    { useNativeDriver: true }
+                )}
+                firstItem={0}
+                enableSnap={true}
+                decelerationRate="fast"
+                sliderWidth={VIEWPORT_WIDTH}
+                itemWidth={CARD_WIDTH}
+                containerCustomStyle={styles.scrollView}
+                activeAnimationType={'spring'}
+            />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
