90f3b08b2d9194fa0bdf0cea60176e624f17b4f4
duranhumes <duranhumes@gmail.com>
2019-02-02 05:28:04 -0500

    Adjusted docker config && removed unnecessary files

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -2,19 +2,19 @@ module.exports = {
     apps: [
         {
             name: 'api',
-            script: './build/index.js',
+            script: './build/src/index.js',
             watch: true,
             instances: 4,
             exec_mode: 'cluster',
             max_restarts: 5,
             env: {
-                PORT: 8080,
+                PORT: 8000,
                 NODE_ENV: 'development',
             },
             env_production: {
-                PORT: 8080,
+                PORT: 8000,
                 NODE_ENV: 'production',
             },
         },
     ],
-}
+};
--- api/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `new-prototype-app` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-CREATE DATABASE IF NOT EXISTS `new-prototype-app-test` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-USE `new-prototype-app`;
